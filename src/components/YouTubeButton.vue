f91234eb0ae0dc1d056786c7b312695ef08c3d6d
sekar N <sekarnagarajan1234@gmail.com>
2023-05-09 12:37:17 +0530

    vuejs

---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="mb-3 px-2 py-1 rounded border-2 text-white border-red-600 bg-red-600 hover:bg-red-600 transition-colors"
+    ><slot>YouTube</slot></a
+  >
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
+
+<style></style>
